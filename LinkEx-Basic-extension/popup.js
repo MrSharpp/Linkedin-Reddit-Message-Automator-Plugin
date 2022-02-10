6b78593a6c6f7b943b72f378928262e6c75df87f
stewpeed <yammaar9@gmail.com>
2022-02-10 23:56:03 +0530

    Update

--- LinkEx-Basic-extension/popup.js
+++ LinkEx-Basic-extension/popup.js
@@ -11,57 +11,27 @@ let totalNum = 0;
 
 
 $(".follow").click(function(){
-    let query = $(".follow-query").val().replace(/ /g,'');
-    query = encodeURIComponent(query);
-    let people = $(".follow-select").val();
-    // validate input here man
-    localStorage.setItem("all", 0);
 
-    // url: `https://www.instagram.com/explore/people/suggested/#startfollow${people}&name=${query}`
-    chrome.tabs.create({
-        url: `https://www.instagram.com/${query}?startfollow&num=${people}`
-    })
 })
 
 
 $(".all").click(function(){
-    let query = $(".follow-query-all").val().replace(/ /g,'');
-    query = encodeURIComponent(query);
-    let people = $(".follow-select").val();
-    let msg = $('.msg-query').val()
 
-
-    chrome.tabs.create({
-        url: `https://www.instagram.com/${query}?startall&num=${people}&all=1&msg=${msg}&u=${query}`
-    })
 })
 
 chrome.runtime.sendMessage({type: GET_TEMP_COMMENT}, (res)=>{
-    if(res && res.tempComment) $(".comment-text").val(res.tempComment);
 })
 
 chrome.runtime.sendMessage({type: GET_MESSAGE}, (res)=>{
-    if(res && res.message) $(".messenger-text").val(res.message);
 })
 
 chrome.storage.local.get(FOLLOW_IDS_KEY, (res)=>{
-    if(res && res[FOLLOW_IDS_KEY]){
-        let followIds = res[FOLLOW_IDS_KEY];
-        if(!followIds) followIds = {};
-        let queries = Object.keys(followIds);
-        let queriesHTML = queries.map(query=>{
-            // return `<div class="tag is-medium">${query}</div>`
-            return `<button ref-query="${query}" class="button is-outlined">${query} <div class="tag ml-3">${followIds[query].length}</div></button>`
-        });
-        // document.querySelector(".queries").innerHTML = queriesHTML.join(" ");
-    }
+
 })
 
 
 
 function getToday() {
-    let ndate = new Date();
-    return `${ndate.getFullYear()}-${ndate.getMonth()}-${ndate.getDate()}`;
 }
 
 function newActivity() {
@@ -81,65 +51,20 @@ chrome.storage.local.get(['userIds'], function(result) {
 
 
 chrome.storage.sync.get(ACTIVITY_TRACK, (resp) => {
-    let activites = resp[ACTIVITY_TRACK];
-    if(!activites || activites.date != getToday()){
-        activites = newActivity();
-        chrome.storage.sync.set({[ACTIVITY_TRACK]: activites});
-    }
-    // $(".follow-use").text(`${localStorage.getItem('userIds')}`)
-
-    $(".follow-use").text(`${activites[ACTIVITY_NAMES.FOLLOWS]}/50`)
-    $(".tweet-use").text(`${activites[ACTIVITY_NAMES.TWEETS]}/50`)
-    $(".messages-use").text(`${activites[ACTIVITY_NAMES.MESSAGES]}/50`)
 })
 
 
 
 $(document).on("click", ".queries .button", function(){
-    let tag = $(this).find(".tag");
-    let query = $(this).attr("ref-query");
 
-    if(tag.hasClass("is-link")){
-        tag.removeClass("is-link");
-        selectedQueries.delete(query);
-        totalNum -= parseInt(tag.text());
-    }else{
-        tag.addClass("is-link");
-        selectedQueries.add(query);
-        totalNum += parseInt(tag.text());
-    }
-    $(".totalnum").text(totalNum);
 })
 
 $("#msg").click(function(){
-    let message = $(".messenger-text").val();
-    let num = $(".dm-select").val();
-    let q = $(".dm-query").val();
 
-    chrome.runtime.sendMessage({type: SET_MESSAGE, message});
-    chrome.runtime.sendMessage({type: SEND_MESSAGES, selectedQueries: [...selectedQueries]});
-
-    chrome.tabs.create({
-        url:   `https://www.instagram.com/${q}/?startinbox&msg=${message}&num=${num}&uname=${q}`
-    })
-
-    // chrome.tabs.create({
-    //     url:   `https://www.instagram.com/direct/inbox/#startinbox&msg=${message}&num=${num}&startfrom=0`
-    // })
-    localStorage.setItem("all", 0);
 })
 
 $(".comment").click(function(){
-    let query = $(".like-query").val();
-    query = encodeURIComponent(query);
-    let people = $(".comment-select").val();
-    let comment = $(".comment-text").val();
-    chrome.runtime.sendMessage({type: SET_TEMP_COMMENT, tempComment: comment});
-    localStorage.setItem("all", 0);
 
-    chrome.tabs.create({
-        url:   `https://www.instagram.com/${query}?startlike&num=${people}`
-    })
 })
 
 $("[ref-tab]").click(function(e){
